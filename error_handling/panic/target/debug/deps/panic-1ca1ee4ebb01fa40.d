abf04a28b903ce814b3b5e3e0e2d5680b1c6529f
Helgard Richard Ferreira <hrfer3@gmail.com>
2020-03-02 10:30:30 +0200

    Unrecoverable Errors with panic!

---
+++ error_handling/panic/target/debug/deps/panic-1ca1ee4ebb01fa40.d
@@ -0,0 +1,5 @@
+/Users/helgardrichardferreira/code/rust/rust_programming_language_exercises/error_handling/panic/target/debug/deps/panic-1ca1ee4ebb01fa40: src/main.rs
+
+/Users/helgardrichardferreira/code/rust/rust_programming_language_exercises/error_handling/panic/target/debug/deps/panic-1ca1ee4ebb01fa40.d: src/main.rs
+
+src/main.rs:
